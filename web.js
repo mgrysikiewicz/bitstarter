a69910da10b42ec502f3abd92a77f241551db393
Michael Grysikiewicz <mgrysikiewicz@gmail.com>
2013-07-11 02:57:04 +0000

    Changed output to come from index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = requre('fs');
+var infile = "index.html";
+var fileContents = fs.readFileSync(infile);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fileContents.toString);
 });
 
 var port = process.env.PORT || 5000;
